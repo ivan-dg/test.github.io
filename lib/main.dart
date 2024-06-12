43d544532799b4d65a533eb4779df9b43bc01989
Ivan G <43581673+ivan-dg@users.noreply.github.com>
2024-06-12 14:37:24 -0500

    deploy

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:briefcase/lib/pages/home_page.dart';
 import 'package:briefcase/lib/pages/mepet_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -38,7 +39,7 @@ class MainApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const MepetPage(),
+      home: const HomePage(),
     );
   }
 }
